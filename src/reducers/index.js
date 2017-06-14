9877b38e3d4d0d0f8c1e7bc992115a20a5eae048
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-06-14 12:00:38 +0700

    Consuming Actions in Reducers Continued

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
-import activeBook from './reducer_active_book';
+import ActiveBook from './reducer_active_book';
 
 const rootReducer = combineReducers({
   // state: (state = {}) => state
